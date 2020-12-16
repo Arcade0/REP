2d4ff31476df5c3ac742a415edc8c12eee3dbc35
xinzhuo123 <xinzhuo12345678@163.com>
2020-12-16 20:45:48 +0800

    almost

--- 1.Search/pubmed_search.py
+++ 1.Search/pubmed_search.py
@@ -2,9 +2,13 @@ import numpy as np
 from collections import Counter
 import os
 import json
+import time
+from random import randint
 from Bio import Medline, Entrez  # 一般是通过BioPython的Bio.Entrez模块访问Entrez
+from func_timeout import func_set_timeout
 
 Entrez.email = "（xinzhuo12345678@163.com）"  # 应用自己的账号访问NCBI数据库
+Entrez.max_tries = 20
 
 # 此处需将服务器协议指定为1.0，否则会出现报错。http.client.IncompleteRead: IncompleteRead(0 bytes read)
 # 服务器http协议1.0，而python的是1.1，解决办法就是指定客户端http协议版本
@@ -18,7 +22,7 @@ def mk_dir(file_path):
     if not folder:
         os. makedirs(file_path)
 
-def query(input_path, output_path, keyword, 
+def query(input_path, output_path, keyword, keyp="Mesh",
           date="(1900/01/01[Date - Publication] : 2021/12/31[Date - Publication])", 
           ret_max=100, step=100, 
           fur_query=False):
@@ -28,64 +32,77 @@ def query(input_path, output_path, keyword,
     获得有关 global PBDE 的所有文献的PubMed IDs
     """
 
-    sp_l = [ele for ele in os.listdir(input_path) if ".ipynb" not in ele]
+    sp_l = [ele for ele in os.listdir(input_path) if "_done" not in ele]
+    sp_l = [ele for ele in sp_l if ".ipynb" not in ele]
 
     for sp in sp_l:
         
         print(sp)
-        with open("%s/%s_done.txt" % (input_path, sp.replace(".txt", "")), "a") as df:
+         # print(sp)
+
+        with open("%s/%s" % (input_path, sp.replace(".txt", "_done.txt")), "a") as df:
             df.close()
         
-        with open("%s/%s_done.txt" % (input_path, sp.replace(".txt", "")), "r") as df:
+        with open("%s/%s" % (input_path, sp.replace(".txt", "_done.txt")), "r") as df:
 
             done_l = df.readlines()
-            done_l = [ele.replace("\n", "") for ele in done_l ]
+            done_l = [ele.replace("\n", "") for ele in done_l]
             df.close()
-        
+
         with open("%s/%s" % (input_path, sp), "r") as f:
 
             spec_l = f.readlines()
-            spec_l = [ele.replace("\n", "") for ele in spec_l if ele not in done_l ]
+            spec_l = [ele.replace("\n", "") for ele in spec_l if ele.replace("\n", "") not in done_l]
             f.close()
         
         for spec in spec_l:
 
-            print(spec)   
             mk_dir("%s/%s/%s" % (output_path, sp.replace(".txt", ""), spec.replace(" ", "_"))) # 保存格式 类/种名
-            term_l = [spec + " AND " + ele for ele in keyword]
-            term_l.append(spec)
+            
+            if keyp == "Mesh":
+
+                term_l = [spec + " AND " + ele for ele in keyword] + \
+                    [spec + "[Mesh]" + " AND " + ele for ele in keyword]
+                term_l.append(spec + "[Mesh]")
+            
+            else:
+                term_l = [spec + " AND " + ele for ele in keyword]
+                term_l.append(spec + "[Mesh]")
+            
         
             for term in term_l:
-            
+
+                output_name = "%s/%s/%s/%s.txt" % (
+                        output_path, sp.replace(".txt", ""), 
+                        spec.replace(" ", "_"), term.replace(" ", "_"))
+
+
                 handle_0 = Entrez.esearch(
                     db = "pubmed", 
                     term = term + " AND " + date, # ptyp="Review", 
                     usehistory="y", 
                     retmax=ret_max)
-
+                
                 record = Entrez.read(handle_0)  # 获取检索条件的所有文献
                 idlist = record["IdList"]  # 提取出文献id
                 webenv = record['WebEnv']
                 query_key = record['QueryKey']
                 No_Papers = len(idlist)
-
                 total = No_Papers
+
                 print (term + " Total: ", record["Count"])
-                
+                handle_0.close()
+                time.sleep(randint(5, 20))
+
                 if len(idlist) > 0:
-                    
-                    with open("%s/%s/%s/%s.txt" % (
-                        output_path,
-                        sp.replace(".txt", ""), 
-                        spec.replace(" ", "_"), 
-                        term.replace(" ", "_")), "w") as f: # save pmid
+
+                    with open(output_name, "w") as f: # save pmid
                         
                         for ele in idlist:
                             
                             f.write(ele + "\n")
-                        
-                        f.close()
-
+                        f.close()   
+               
                     if fur_query==True:
                         
                         dic = {}
@@ -102,23 +119,20 @@ def query(input_path, output_path, keyword,
                             records = list(records)
                             dic.update({ele["PMID"]:ele for ele in records})
 
-                        with open("%s/%s/%s/%s.json" % (
-                            output_path,
-                            sp.replace(".txt", ""), 
-                            spec.replace(" ", "_"), 
-                            term.replace(" ", "_")), 'w') as f: # save json file
+                        with open(output_name.replace("txt", "json"), 'w') as f: # save json file
                 
                                 js = json.dumps(dic)
                                 f.write(js)
                                 f.close()
+                    else:
+                        pass
                 else:
                     pass
-        
-            with open("%s/%s_done.txt" % (input_path, sp.replace(".txt", "")), "a") as df:
+
+            with open("%s/%s" % (input_path, sp.replace(".txt", "_done.txt")), "a") as df:
 
                 df.write(spec + "\n")
-                df.close()
-
+                df.close()     
 
 # 不使用python直接运行这个文件，会导致卡死，原因未知...
 if __name__ == "__main__":
--- 2.PubTator/SubmitPMIDList.py
+++ 2.PubTator/SubmitPMIDList.py
@@ -2,27 +2,31 @@ import requests
 import io
 import json
 import sys
+from requests.adapters import HTTPAdapter
+HTTPAdapter(max_retries=10)
 
 def SubmitPMIDList(Inputfile,Format,Outfile,Bioconcept):
 	
 	json = {}
 	
-	#
 	# load pmids
-	#
 	with io.open(Inputfile,'r',encoding="utf-8") as file_input:
 		json = {"pmids": [pmid.strip() for pmid in file_input.readlines()]}
 	
-	#
 	# load bioconcepts
-	#
 	if Bioconcept != "": 
 		json["concepts"]=Bioconcept.split(",")
         
-	#
 	# request
-	#
-	r = requests.post("https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/"+Format , json = json)
+	r = requests.post(
+		"https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/"+Format, 
+		json = json)
+	
+	# r = requests.post(
+	# 	"https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/"+Format, 
+	# 	json = json,
+	# 	timeout= (120, 120))
+
 	if r.status_code != 200 :
 		print ("[Error]: HTTP code "+ str(r.status_code))
 	else:
@@ -31,25 +35,3 @@ def SubmitPMIDList(Inputfile,Format,Outfile,Bioconcept):
 		outputfile = open(Outfile, 'w')
 		outputfile.write(response)
 		outputfile.close()
-
-if __name__ == "__main__":
-
-	arg_count=0
-	for arg in sys.argv:
-		arg_count+=1
-	if arg_count<3 or (sys.argv[2]!= "pubtator" and sys.argv[2]!= "biocxml" and sys.argv[2]!= "biocjson"):
-		print("\npython SubmitPMIDList.py [InputFile] [Format] [BioConcept]\n\n")
-		print("\t[Inputfile]: a file with a pmid list\n")
-		print("\t[Format]: pubtator (PubTator), biocxml (BioC-XML), and biocjson (JSON-XML)\n")
-		print("\t[Bioconcept]: gene, disease, chemical, species, proteinmutation, dnamutation, snp, and cellline. Default includes all.\n")
-		print("\t* All input are case sensitive.\n\n")
-		print("Eg., python SubmitPMIDList.py examples/ex.pmid pubtator gene,disease\n\n")
-	else:
-		Inputfile = sys.argv[1]
-		Format = sys.argv[2]
-		Outfile = sys.argv[3]
-		Bioconcept=""
-		if arg_count>=5:
-			Bioconcept = sys.argv[4]
-		
-		SubmitPMIDList(Inputfile,Format,Outfile,Bioconcept)
